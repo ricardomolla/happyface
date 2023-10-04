b8d4f7ae65988b012cb738970e08d09aa11a4af6
ricardomolla <146949613+ricardomolla@users.noreply.github.com>
2023-10-04 18:42:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+let enfadado = false; // Una variable para determinar si la cara fue clicada
+
 document.querySelector(".cara").addEventListener("mouseover", function() {
-    this.classList.remove('feliz', 'enfado');
-    this.classList.add('triste');
+    if (!enfadado) {
+        this.classList.remove('feliz', 'enfado');
+        this.classList.add('triste');
+    }
 });
 
 document.querySelector(".cara").addEventListener("mouseout", function() {
-    this.classList.remove('triste', 'enfado');
-    this.classList.add('feliz');
+    if (!enfadado) {
+        this.classList.remove('triste', 'enfado');
+        this.classList.add('feliz');
+    }
 });
 
 document.querySelector(".cara").addEventListener("click", function() {
     this.classList.remove('feliz', 'triste');
     this.classList.add('enfado');
+    enfadado = true; // Al hacer clic, se activa el modo enfado
 });
+
+// Si deseas que la cara vuelva a su estado normal después de algún tiempo cuando está enfadada, puedes agregar:
+setTimeout(function() {
+    let cara = document.querySelector(".cara");
+    if (cara.classList.contains('enfado')) {
+        cara.classList.remove('enfado');
+        cara.classList.add('feliz');
+        enfadado = false; // Se desactiva el modo enfado
+    }
+}, 3000); // Aquí, la cara volverá a su estado feliz después de 3 segundos. Puedes ajustar el tiempo si lo prefieres.
