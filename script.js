a2f705d3ebb232bac051f61663794d59259e4a9e
ricardomolla <146949613+ricardomolla@users.noreply.github.com>
2023-10-04 18:37:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.querySelector(".cara").addEventListener("mouseover", function() {
-  this.classList.remove('feliz', 'enfado');
-  this.classList.add('triste');
+    this.classList.remove('feliz', 'enfado');
+    this.classList.add('triste');
 });
 
 document.querySelector(".cara").addEventListener("mouseout", function() {
-  this.classList.remove('triste', 'enfado');
-  this.classList.add('feliz');
+    this.classList.remove('triste', 'enfado');
+    this.classList.add('feliz');
 });
 
 document.querySelector(".cara").addEventListener("click", function() {
-  this.classList.remove('feliz', 'triste');
-  this.classList.add('enfado');
+    this.classList.remove('feliz', 'triste');
+    this.classList.add('enfado');
 });
